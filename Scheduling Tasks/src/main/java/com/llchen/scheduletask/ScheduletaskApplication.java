fd00b2dee721670843feb6baff1dae3ff627b6da
stone16 <stone2paul@gmail.com>
2018-12-01 14:58:11 -0800

    scheduling tasks

--- Scheduling Tasks/src/main/java/com/llchen/scheduletask/ScheduletaskApplication.java
+++ Scheduling Tasks/src/main/java/com/llchen/scheduletask/ScheduletaskApplication.java
@@ -2,8 +2,10 @@ package com.llchen.scheduletask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ScheduletaskApplication {
 
     public static void main(String[] args) {
