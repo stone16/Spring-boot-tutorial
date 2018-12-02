841427ad4a72401b478511f42538d1bdb874aa20
stone16 <stone2paul@gmail.com>
2018-12-01 17:08:24 -0800

    Access relational data using JDBC with Spring

---
+++ Accessing Relational Data using JDBC with Spring/src/main/java/com/llchen/accessdata/Customer.java
@@ -0,0 +1,24 @@
+package com.llchen.accessdata;
+
+import lombok.Data;
+
+// Represend the customer data at the application level
+@Data
+public class Customer {
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+}
