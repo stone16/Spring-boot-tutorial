b6d20c89d4eab9ec5900c10fad1037b5a24abbfa
stone16 <stone2paul@gmail.com>
2018-12-09 18:09:19 -0800

    Messaging with JMS

---
+++ Messaging with JMS/src/main/java/com/llchen/message/MessageApplication.java
@@ -0,0 +1,51 @@
+package com.llchen.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import javax.jms.ConnectionFactory;
+
+/**
+ * Walk through the process of publishing and subscribing to messages using a JMS broker
+ */
+@SpringBootApplication
+// triggers the discovery of methods annotated with @jmsListener, creating the message listener container under the vocers
+@EnableJms
+public class MessageApplication {
+
+    @Bean
+    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+                                                    DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        // This provides all boot's default to this factory, including the message converter
+        configurer.configure(factory, connectionFactory);
+        // You could still override some of Boot's default if necessary.
+        return factory;
+    }
+
+    @Bean // Serialize message content to json using TextMessage
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(MessageApplication.class, args);
+
+        JmsTemplate jmsTemplate = ctx.getBean(JmsTemplate.class);
+
+        jmsTemplate.convertAndSend("mailbox", new Email("info@example.com", "Hello"));
+    }
+}
